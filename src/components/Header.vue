e3f8d803e14939da81f165b94a8db360414e9e07
Hamza El yousfi <58729627+hamzael94-gif@users.noreply.github.com>
2022-01-19 15:48:08 +0100

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button
+            @btn-click="$emit('toggel-add-task')"
+            :color="ShowAddTask ?'red':'green'" 
+            :text="ShowAddTask ? 'Close':'Add task'"
+        />
+    </header>
+</template>
+<script>
+import Button from './Button.vue'
+export default {
+    name:"Header",
+    props:{
+        title:String,
+        ShowAddTask :Boolean
+    },
+    components:{
+        Button,
+    },
+}
+</script>
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
